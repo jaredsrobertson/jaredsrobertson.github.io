93106a5d1362bbabd536487169d5ab798d9c3e3a
Jared Robertson <128831437+jaredsrobertson@users.noreply.github.com>
2023-07-07 15:51:56 -0500

    Testing JS functionality

--- myScript.js
+++ myScript.js
@@ -5,3 +5,43 @@ function lightMode() {
     }
 }
 
+function btnHom() {
+    document.getElementsByClassName("pghome")[0].setAttribute('style', 'display:block');
+    document.getElementsByClassName("pgskills")[0].setAttribute('style', 'display:none');
+    document.getElementsByClassName("pgexpedu")[0].setAttribute('style', 'display:none');
+    document.getElementsByClassName("pgprojects")[0].setAttribute('style', 'display:none');
+    document.getElementsByClassName("pgcontact")[0].setAttribute('style', 'display:none');
+}
+
+
+function btnSki() {
+    document.getElementsByClassName("pgskills")[0].setAttribute('style', 'display:block');
+    document.getElementsByClassName("pghome")[0].setAttribute('style', 'display:none');
+    document.getElementsByClassName("pgexpedu")[0].setAttribute('style', 'display:none');
+    document.getElementsByClassName("pgprojects")[0].setAttribute('style', 'display:none');
+    document.getElementsByClassName("pgcontact")[0].setAttribute('style', 'display:none');
+}
+
+function btnExp() {
+    document.getElementsByClassName("pgexpedu")[0].setAttribute('style', 'display:block');
+    document.getElementsByClassName("pghome")[0].setAttribute('style', 'display:none');
+    document.getElementsByClassName("pgskills")[0].setAttribute('style', 'display:none');
+    document.getElementsByClassName("pgprojects")[0].setAttribute('style', 'display:none');
+    document.getElementsByClassName("pgcontact")[0].setAttribute('style', 'display:none');
+}
+
+function btnPro() {
+    document.getElementsByClassName("pgprojects")[0].setAttribute('style', 'display:block');
+    document.getElementsByClassName("pghome")[0].setAttribute('style', 'display:none');
+    document.getElementsByClassName("pgskills")[0].setAttribute('style', 'display:none');
+    document.getElementsByClassName("pgexpedu")[0].setAttribute('style', 'display:none');
+    document.getElementsByClassName("pgcontact")[0].setAttribute('style', 'display:none');
+}
+
+function btnCon() {
+    document.getElementsByClassName("pgcontact")[0].setAttribute('style', 'display:block');
+    document.getElementsByClassName("pghome")[0].setAttribute('style', 'display:none');
+    document.getElementsByClassName("pgskills")[0].setAttribute('style', 'display:none');
+    document.getElementsByClassName("pgexpedu")[0].setAttribute('style', 'display:none');
+    document.getElementsByClassName("pgprojects")[0].setAttribute('style', 'display:none');
+}
